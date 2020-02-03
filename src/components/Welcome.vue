10da009ad8b930269ce589cbdc5a86d3b175444c
pankaj7822 <pankaj.krjha.cse17@itbhu.ac.in>
2020-02-03 14:22:05 +0530

    corrected spelling

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-card elevation="3" max-width="1000" class="mx-auto">
       <v-row>
-        <v-col cols="12" sm="12" md="3" lg="4">
+        <v-col cols="12" sm="12" md="4" lg="4">
           <v-img
             style="border-radius:100%;"
             :src="guest"
@@ -10,11 +10,11 @@
             width="400"
           ></v-img>
         </v-col>
-        <v-col cols="12" sm="12" md="6" lg="4">
+        <v-col cols="12" sm="12" md="8" lg="4">
           <v-card-title class="text-justify pa-3">Welcome Message</v-card-title>
           <v-card-text class="text-justify pa-3">{{ message }}</v-card-text>
         </v-col>
-        <v-col cols="12" sm="12" md="3" lg="4">
+        <v-col cols="12" sm="12" md="4" lg="4">
           <v-img
             :src="nobel"
             style="border-radius:100%;"
@@ -33,7 +33,7 @@ export default {
       guest: require("@/assets/images/welcome/goodenough.png"),
       nobel: require("@/assets/images/welcome/Nobel_Prize.png"),
       message:
-        "The aim of the conference is to celebrate the recognition of Li-ion battery technology and pay tribute to the (living legend and battery pioneer Prof. John B. Goodenough (Nobel Prize in Chemistry 2019 The conference is themed on the emergence of hydrogen economy in the arena of post fossil fuel technologies and emerging zero emission technologies      especially for automobile industries. Filling the gap between fuel cells and rechargeable atteries technologies to overcome the challenges imposed by existing fossil fuel based technologies are the main agendas of the conference"
+        "The aim of the conference is to celebrate the recognition of Li-ion battery technology and pay tribute to the (living legend and battery pioneer Prof. John B. Goodenough (Nobel Prize in Chemistry 2019 The conference is themed on the emergence of hydrogen economy in the arena of post fossil fuel technologies and emerging zero emission technologies      especially for automobile industries. Filling the gap between fuel cells and rechargeable batteries technologies to overcome the challenges imposed by existing fossil fuel based technologies are the main agendas of the conference"
     };
   }
 };
